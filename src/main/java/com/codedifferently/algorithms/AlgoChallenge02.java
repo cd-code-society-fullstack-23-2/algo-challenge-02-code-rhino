99decf41978d170c29e104679296546c735708bd
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2023-09-06 10:46:40 -0400

    complete

--- src/main/java/com/codedifferently/algorithms/AlgoChallenge02.java
+++ src/main/java/com/codedifferently/algorithms/AlgoChallenge02.java
@@ -4,7 +4,19 @@ public class AlgoChallenge02 {
 
     public String encrypt(String message, int shift){
 
-        return null;
+        // Convert the string to a char array
+        char[] chars = message.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            // Check if it's an alphabetic character
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                chars[i] = (char) ((c + shift - base) % 26 + base);
+            }
+        }
+
+        return new String(chars);
 
     }
 }
